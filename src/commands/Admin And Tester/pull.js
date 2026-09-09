9c8a50d1ca424a04ca1dcb17df9bffa29a783975
CurioSitzy <aqelafitri1@gmail.com>
2026-09-09 11:51:16 +0700

    Update pull.js

--- src/commands/Admin And Tester/pull.js
+++ src/commands/Admin And Tester/pull.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, ChannelType, PermissionFlagsBits } from 'discord.js';
-import { waitlistService } from '../services/WaitlistService.js';
+import { waitlistService } from '../../services/waitlistservice.js';
 
 export default {
     data: new SlashCommandBuilder()
@@ -19,21 +19,20 @@ export default {
         }
 
         const guild = interaction.guild;
-        const categoryId = process.env.TICKET_CATEGORY_ID; // Set your ticket category ID in .env if available
+        const categoryId = process.env.TICKET_CATEGORY_ID;
 
         try {
-            // Create a private ticket channel for the Player & Tester
             const ticketChannel = await guild.channels.create({
                 name: `ticket-${player.username}`,
                 type: ChannelType.GuildText,
                 parent: categoryId || null,
                 permissionOverwrites: [
                     {
-                        id: guild.id, // Hide from everyone (@everyone)
+                        id: guild.id,
                         deny: [PermissionFlagsBits.ViewChannel]
                     },
                     {
-                        id: player.id, // Permissions for Player
+                        id: player.id,
                         allow: [
                             PermissionFlagsBits.ViewChannel,
                             PermissionFlagsBits.SendMessages,
@@ -41,7 +40,7 @@ export default {
                         ]
                     },
                     {
-                        id: interaction.user.id, // Permissions for Tester
+                        id: interaction.user.id,
                         allow: [
                             PermissionFlagsBits.ViewChannel,
                             PermissionFlagsBits.SendMessages,
@@ -51,10 +50,8 @@ export default {
                 ]
             });
 
-            // Save ticket data
             waitlistService.registerTicket(ticketChannel.id, player, interaction.user.id);
 
-            // Send welcome message in ticket channel
             await ticketChannel.send({
                 content: `Hello <@${player.id}>! Your testing ticket has been created by Tester <@${interaction.user.id}>.\nUse \`/close\` once the testing session is finished.`
             });
