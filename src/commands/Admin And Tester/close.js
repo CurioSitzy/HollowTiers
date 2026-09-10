4a2989278f8583a6e53228913afc17a08cd3981c
CurioSitzy <aqelafitri1@gmail.com>
2026-09-10 19:02:25 +0700

    Update close.js

--- src/commands/Admin And Tester/close.js
+++ src/commands/Admin And Tester/close.js
@@ -24,15 +24,11 @@ export default {
       });
     }
 
-    // 2. Ticket Channel Validation
-    let isTicket = false;
-    if (typeof waitlistService.getTicket === 'function') {
-      isTicket = Boolean(waitlistService.getTicket(channel.id));
-    } else {
-      isTicket = channel.name.startsWith('ticket-');
-    }
+    // 2. Validate Ticket Channel via waitlistService
+    const ticketData = waitlistService.getTicket(channel.id);
+    const isChannelByPrefix = channel.name.startsWith('ticket-');
 
-    if (!isTicket) {
+    if (!ticketData && !isChannelByPrefix) {
       return interaction.reply({
         content: '❌ This command can only be used inside an active testing ticket channel.',
         flags: MessageFlags.Ephemeral
@@ -43,10 +39,8 @@ export default {
       content: '🔒 Ticket is closing and this channel will be deleted in 5 seconds...'
     });
 
-    // 3. Remove Ticket from Waitlist Memory
-    if (typeof waitlistService.removeTicket === 'function') {
-      waitlistService.removeTicket(channel.id);
-    }
+    // 3. Clean Memory
+    waitlistService.removeTicket(channel.id);
 
     // 4. Delete Channel
     setTimeout(async () => {
